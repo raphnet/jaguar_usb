9dad62c5cfabe28b204b7c5a86503ae1811a56e1
Raphael Assenat <raph@raphnet.net>
2016-11-20 16:40:51 -0500

    Version 1.2

--- usbconfig.h
+++ usbconfig.h
@@ -132,7 +132,7 @@ rename it to "usbconfig.h". Then edit it accordingly.
 #define  USB_CFG_VENDOR_ID       0x9B, 0x28
 #define  USB_CFG_DEVICE_ID       0x18, 0x00
 
-#define USB_CFG_DEVICE_VERSION  0x01, 0x01
+#define USB_CFG_DEVICE_VERSION  0x02, 0x01
 /* Version number of the device: Minor number first, then major number.
  */
 #define USB_CFG_VENDOR_NAME     'r', 'a', 'p', 'h', 'n', 'e', 't', '.', 'n', 'e', 't'
